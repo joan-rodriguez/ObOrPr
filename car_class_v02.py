93f934dc940e031adb2fa9e42560800a59c1bb7c
joan <joan.rodriguezre@gmail.com>
2019-11-01 12:05:49 +0100

    Car works! input() error :S

--- car_class_v02.py
+++ car_class_v02.py
@@ -10,7 +10,7 @@ class Car:
         self.max_tank = 50  # liters
         self.tank = random.randint(0, self.max_tank)  # liters
         self.color = random.choice(self.possible_colors)
-        self.consumption = 7  # liters/100km
+        self.consumption = 0.07  # liters/km
         self.distance = 0  # km
 
     def turn_on(self):
@@ -22,20 +22,23 @@ class Car:
         print('\nShhhhhhh... Engine is off now!')
 
     def move(self):
-        max_dist = (self.tank / self.consumption) / 100  # km
+        max_dist = round(self.tank / self.consumption, 2)  # km
+        print('Current max dist is: {}'.format(max_dist))
         while True:
-            dist = str(input('\nHow many km would you like to travel? --> '))
+            dist = input('\nHow many km would you like to travel? --> ')
             if not self.engine:
                 print('\nI am not an expert, but I think you should turn the engine on first...')
                 break
-            if dist.isdigit():
+            if str(dist).isdigit():
                 if int(dist) > max_dist:
                     self.distance += max_dist
+                    self.tank -= round(max_dist * self.consumption, 2)
                     print('\nYour tank is empty. Your car just moved {}km. Total distance is {}km.'
-                          .format(dist, self.distance))
+                          .format(max_dist, self.distance))
                     break
                 else:
                     self.distance += dist
+                    self.tank -= round(float(dist) * self.consumption, 2)
                     print('\nYour car moved {}km! Total distance is {}km.'.format(dist,
                                                                                   self.distance))
                     break
@@ -43,13 +46,13 @@ class Car:
                 print('You need to input a number of km you want to travel.')
 
     def refill(self):
-        max_liters = self.max_tank - self.tank
+        max_liters = round(self.max_tank - self.tank, 2)
         while True:
             liters = input('How many liters do you want to refill? --> ')
             if self.engine:
                 print('Hey! Don\'t fill in your tank while... KABOOOOOM!!')
                 exit()
-            if liters.isdigit():
+            if str(liters).isdigit():
                 if int(liters) > max_liters:
                     self.tank = self.max_tank
                     print('\nYou could only fill {}liters in your tank. Now it is full!'.format(max_liters))
@@ -61,7 +64,7 @@ class Car:
     def paint(self):
         print('\nYour car\'s color is currently {}.'.format(self.color))
         while True:
-            new_color = input('What color would you liketo paint your car off? --> ')
+            new_color = input('What color would you like to paint your car off? --> ')
             if new_color == self.color:
                 print('\nPlease... Don\'t waste my time...')
                 break
@@ -71,7 +74,7 @@ class Car:
                 break
             else:
                 print('Oooooooh!! This color just got finished... Next week we\'ll have more'
-                      'in store. Until then...')
+                      ' in store. Until then...')
 
     def show_status(self):
         print("""
@@ -92,32 +95,32 @@ class Manager:
         self.options = {1: car.show_status, 2: car.turn_on, 3: car.turn_off,
                         4: car.move, 5: car.refill, 6: car.paint}
 
-    def display_menu(self):
+    @staticmethod
+    def display_menu():
         print("""
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-      ----> Menu <----
+|      ----> Menu <----     |
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-1- Check out car status
-2- Turn engine ON
-3- Turn engine OFF
-4- Run
-5- Refill tank
-6- Paint it
-
-7- Exit
+| 1- Check out car status   |
+| 2- Turn engine ON         |
+| 3- Turn engine OFF        |
+| 4- Run                    |
+| 5- Refill tank            |
+| 6- Paint it               |
+|                           |
+| 7- Exit                   |
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 """)
 
     def run(self):
-        self.display_menu()
-
         while True:
-            option = input('\nWhat do you want to do with your car? --> ')
+            self.display_menu()
+            option = input('What do you want to do with your car? --> ')
             if option == 7:
                 print('\nLeaving the car in the road, you walk towards the setting Sun...')
                 exit()
 
-            action = self.options.get(option)
+            action = self.options.get(int(option))
             if action:
                 action()
             else:
